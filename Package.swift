23b389d850160ceedaa6d2195281b399a5dc1b5a
JungDohyeon <jdh1109ok@naver.com>
2024-09-01 16:44:47 +0900

    [✨ Feat] Add External Dependencies
    Alamofire, Moya, Swinject, SnapKit

--- Package.swift
+++ Package.swift
@@ -6,17 +6,26 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let packageSetting = PackageSettings(
-    baseSettings: .settings(
-        configurations: [
-            .debug(name: .dev),
-            .debug(name: .stage),
-            .release(name: .prod)
-        ]
-    )
+  productTypes: [
+    "Alamofire": .staticFramework,
+    "Moya": .staticFramework
+  ],
+  baseSettings: .settings(
+    configurations: [
+      .debug(name: .dev),
+      .debug(name: .stage),
+      .release(name: .prod)
+    ]
+  )
 )
 #endif
 
 let package = Package(
-    name: "Package",
-    dependencies: []
+  name: "ZANI",
+  dependencies: [
+    .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.9.1"),
+    .package(url: "https://github.com/Moya/Moya.git", exact: "15.0.3"),
+    .package(url: "https://github.com/Swinject/Swinject.git", exact: "2.9.1"),
+    .package(url: "https://github.com/SnapKit/SnapKit.git", exact: "5.7.1")
+  ]
 )
--- Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
+++ Plugin/DependencyPlugin/ProjectDescriptionHelpers/Dependency+SPM.swift
@@ -1,10 +1,14 @@
 import ProjectDescription
 
 public extension TargetDependency {
-    struct SPM {}
+  struct SPM {}
 }
 
 public extension TargetDependency.SPM {
+  static let Alamofire = TargetDependency.external(name: "Alamofire")
+  static let Moya = TargetDependency.external(name: "Moya")
+  static let Swinject = TargetDependency.external(name: "Swinject")
+  static let SnapKit = TargetDependency.external(name: "SnapKit")
 }
 
 public extension Package {
