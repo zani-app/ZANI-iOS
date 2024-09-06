576fdf60f7fd116dd250dca9e077337eb807293d
JungDohyeon <jdh1109ok@naver.com>
2024-09-07 01:29:41 +0900

    [KAN-16]🔨 Tuist - DesignSystem Resources 경로 추가
    Font, Color를 위한 Resources 경로 추가

--- Projects/UserInterface/DesignSystem/Project.swift
+++ Projects/UserInterface/DesignSystem/Project.swift
@@ -3,13 +3,20 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.module(
-    name: ModulePaths.UserInterface.DesignSystem.rawValue,
-    targets: [
-        .implements(module: .userInterface(.DesignSystem), product: .framework, dependencies: [
+  name: ModulePaths.UserInterface.DesignSystem.rawValue,
+  targets: [
+    .implements(
+      module: .userInterface(.DesignSystem),
+      product: .framework,
+      spec: .init(
+        resources: ["Resources/**"],
+        dependencies: [
           .core(target: .CoreKit)
-        ]),
-        .demo(module: .userInterface(.DesignSystem), dependencies: [
-            .userInterface(target: .DesignSystem)
-        ])
-    ]
+        ]
+      )
+    ),
+    .demo(module: .userInterface(.DesignSystem), dependencies: [
+      .userInterface(target: .DesignSystem)
+    ])
+  ]
 )
