d03bd0a1e46a7b49ad929c46ca587c1ea3feae87
JungDohyeon <jdh1109ok@naver.com>
2024-11-17 16:35:23 +0900

    [ZNI-12]✨ Coordinator Protocol

--- Projects/Feature/BaseFeature/Sources/Coordinator.swift
+++ Projects/Feature/BaseFeature/Sources/Coordinator.swift
@@ -9,6 +9,33 @@
 import Foundation
 
 public protocol Coordinator: AnyObject {
+  var parentCoordinator: Coordinator? { get set }
   var childCoordinators: [Coordinator] { get set }
+  
   func start()
+  func finish()
+  func addChildCoordinator(_ coordinator: Coordinator)
+  func removeChildCoordinator(_ coordinator: Coordinator)
+}
+
+public extension Coordinator {
+  func start() {
+    preconditionFailure("오버라이드 필요")
+  }
+  
+  func finish() {
+    preconditionFailure("오버라이드 필요")
+  }
+  
+  func addChildCoordinator(_ coordinator: Coordinator) {
+    childCoordinators.append(coordinator)
+  }
+  
+  func removeChildCoordinator(_ coordinator: Coordinator) {
+    if let index = childCoordinators.firstIndex(where: { $0 === coordinator }) {
+      childCoordinators.remove(at: index)
+    } else {
+      print("coordinator 삭제 실패: \(coordinator). ")
+    }
+  }
 }
