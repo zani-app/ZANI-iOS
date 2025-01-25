a8fdde849cdfb2777f28dbd4e61abc0c71c91320
JungDohyeon <jdh1109ok@naver.com>
2025-01-26 00:28:00 +0900

    [ZNI-12]💄 AuthTextField UI 변경

--- Projects/UserInterface/DesignSystem/Sources/Components/AuthTextField.swift
+++ Projects/UserInterface/DesignSystem/Sources/Components/AuthTextField.swift
@@ -13,36 +13,17 @@ import CoreKit
 import Combine
 import SnapKit
 
-@frozen
 public enum AuthTextFieldType {
-  case email
-  case password
-  case passwordConfirm
   case nickname
-  
-  public var titleLabel: String {
-    switch self {
-    case .email: return "이메일"
-    case .password: return "비밀번호"
-    case .passwordConfirm: return "비밀번호 확인"
-    case .nickname: return "닉네임"
-    }
-  }
   
   public var placeHolder: String {
     switch self {
-    case .email: "abc@email.com"
-    case .password: "영문, 숫자, 특수문자 포함 8자 이상"
-    case .passwordConfirm: "비밀번호를 다시 한 번 입력해주세요"
     case .nickname: "닉네임을 입력해주세요"
     }
   }
   
   public var maxLength: Int {
     switch self {
-    case .email: 50
-    case .password: 30
-    case .passwordConfirm: 30
     case .nickname: 8
     }
   }
@@ -71,21 +52,17 @@ public class AuthTextField: UIView {
   
   private var cancelBag = CancelBag()
   
-  // MARK: UI Components
-  private var titleLabel: UILabel = {
-    let label = UILabel()
-    label.font = UIFont.ZANIFontType.title2.font
-    label.textColor = DesignSystemAsset.mainGray.color
-    return label
-  }()
-  
-  private let textFieldContainerView = UIView()
+  // MARK: UI Component
   private let textField = PastBlockedTextField()
-  private let rightIcon = UIImage()
-  private let alertLabel = UILabel()
   
   public var type: AuthTextFieldType!
   private var textFieldState = CurrentValueSubject<AuthTextFieldState, Never>(.normal)
+  
+  public var text: String {
+    get {
+      return textField.text ?? ""
+    }
+  }
   
   public var textChanged: AnyPublisher<String?, Never> {
     textField.publisher(for: .editingChanged)
@@ -112,7 +89,6 @@ public class AuthTextField: UIView {
 
 // MARK: Method
 extension AuthTextField {
-  
   private func bindUI() {
     textChanged
       .replaceNil(with: "")
@@ -147,8 +123,6 @@ extension AuthTextField {
   }
   
   private func setUI() {
-    titleLabel.text = self.type.titleLabel
-    
     textField.backgroundColor = .clear
     textField.textColor = .white
     textField.tintColor = DesignSystemAsset.mainYellow.color
@@ -156,42 +130,24 @@ extension AuthTextField {
       string: self.type.placeHolder,
       attributes: [NSAttributedString.Key.foregroundColor : DesignSystemAsset.mainGray.color]
     )
-    textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 14, height: 0))
+    textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 18.5, height: 0))
     textField.leftViewMode = .always
-    textField.layer.cornerRadius = 8
+    textField.layer.cornerRadius = 24
     textField.layer.borderWidth = 1.0
-    textField.backgroundColor = DesignSystemAsset.main1.color
+    textField.backgroundColor = .black
     textField.font = UIFont.ZANIFontType.body1.font
     textField.returnKeyType = .done
-    
-    textFieldContainerView.backgroundColor = DesignSystemAsset.main1.color
   }
   
   private func setLayout() {
-    self.addSubview(textFieldContainerView)
+    self.addSubview(textField)
     self.configureTitleLabel()
   }
   
   private func configureTitleLabel() {
-    self.snp.makeConstraints { make in
-      make.height.equalTo(82)
-    }
-    
-    self.addSubview(titleLabel)
-    titleLabel.snp.makeConstraints { make in
-      make.leading.top.equalToSuperview()
-    }
-    
-    textFieldContainerView.snp.makeConstraints { make in
-      make.top.equalTo(titleLabel.snp.bottom).offset(15)
-      make.leading.trailing.equalToSuperview()
-      make.height.equalTo(48)
-    }
-    
-    textFieldContainerView.addSubview(textField)
     textField.snp.makeConstraints { make in
-      make.top.bottom.equalToSuperview()
-      make.leading.trailing.equalToSuperview()
+      make.leading.trailing.top.bottom.equalToSuperview()
+      make.height.equalTo(48)
     }
   }
 }
